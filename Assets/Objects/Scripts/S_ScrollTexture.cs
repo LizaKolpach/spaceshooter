58926050f332b1a813d7ca82d965d14870c70fbd
LisaK <lizakolpacheva@yandex.ru>
2020-11-21 12:29:47 +0100

    moving bg

---
+++ Assets/Objects/Scripts/S_ScrollTexture.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_ScrollTexture : MonoBehaviour
+{
+    public float scrollx = 0.5f;
+    public float scrolly = 0.5f;
+    
+    void Update()
+    {
+        float offsetX = Time.time * scrollx;
+        float offsetY = Time.time * scrolly;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
